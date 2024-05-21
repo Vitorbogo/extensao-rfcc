60001c0bc0937f4b5fbb4ad37399b58c1c185725
Vitorbogo <vitorbogo@edu.univali.br>
2024-05-21 19:49:39 -0300

    Itens laterias e rotas #7

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -8,93 +8,119 @@ import {
   IonMenu,
   IonMenuToggle,
   IonNote,
-} from '@ionic/react';
+} from '@ionic/react'
 
-import { useLocation } from 'react-router-dom';
-import { archiveOutline, archiveSharp, bookmarkOutline, heartOutline, heartSharp, mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, trashOutline, trashSharp, warningOutline, warningSharp } from 'ionicons/icons';
-import './Menu.css';
+import { useLocation } from 'react-router-dom'
+import {
+  femaleOutline,
+  femaleSharp,
+  heartOutline,
+  heartSharp,
+  helpCircleOutline,
+  helpCircleSharp,
+  homeSharp,
+  mailOutline,
+  newspaperOutline,
+  newspaperSharp,
+  womanOutline,
+  womanSharp,
+} from 'ionicons/icons'
+import './Menu.css'
 
 interface AppPage {
-  url: string;
-  iosIcon: string;
-  mdIcon: string;
-  title: string;
+  url: string
+  iosIcon: string
+  mdIcon: string
+  title: string
 }
 
 const appPages: AppPage[] = [
   {
-    title: 'Inbox',
-    url: '/folder/Inbox',
+    title: 'Página Inicial',
+    url: '/',
     iosIcon: mailOutline,
-    mdIcon: mailSharp
+    mdIcon: homeSharp,
   },
   {
-    title: 'Outbox',
-    url: '/folder/Outbox',
-    iosIcon: paperPlaneOutline,
-    mdIcon: paperPlaneSharp
+    title: 'Câncer de Mama',
+    url: '/cancer/mama',
+    iosIcon: womanOutline,
+    mdIcon: womanSharp,
   },
   {
-    title: 'Favorites',
-    url: '/folder/Favorites',
+    title: 'Câncer do Colo do Útero',
+    url: '/cancer/colo-utero',
+    iosIcon: femaleOutline,
+    mdIcon: femaleSharp,
+  },
+  {
+    title: 'Colaborações',
+    url: '/sobre/colaboracoes',
     iosIcon: heartOutline,
-    mdIcon: heartSharp
+    mdIcon: heartSharp,
   },
   {
-    title: 'Archived',
-    url: '/folder/Archived',
-    iosIcon: archiveOutline,
-    mdIcon: archiveSharp
+    title: 'Notícias',
+    url: '/noticias',
+    iosIcon: newspaperOutline,
+    mdIcon: newspaperSharp,
   },
   {
-    title: 'Trash',
-    url: '/folder/Trash',
-    iosIcon: trashOutline,
-    mdIcon: trashSharp
+    title: 'Sobre',
+    url: '/sobre',
+    iosIcon: helpCircleOutline,
+    mdIcon: helpCircleSharp,
   },
-  {
-    title: 'Spam',
-    url: '/folder/Spam',
-    iosIcon: warningOutline,
-    mdIcon: warningSharp
-  }
-];
+]
 
-const labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+// const labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders']
 
 const Menu: React.FC = () => {
-  const location = useLocation();
+  const location = useLocation()
 
   return (
-    <IonMenu contentId="main" type="overlay">
+    <IonMenu contentId='main' type='overlay'>
       <IonContent>
-        <IonList id="inbox-list">
+        <IonList id='inbox-list'>
           <IonListHeader>Inbox</IonListHeader>
           <IonNote>hi@ionicframework.com</IonNote>
           {appPages.map((appPage, index) => {
             return (
               <IonMenuToggle key={index} autoHide={false}>
-                <IonItem className={location.pathname === appPage.url ? 'selected' : ''} routerLink={appPage.url} routerDirection="none" lines="none" detail={false}>
-                  <IonIcon aria-hidden="true" slot="start" ios={appPage.iosIcon} md={appPage.mdIcon} />
+                <IonItem
+                  className={
+                    location.pathname === appPage.url ? 'selected' : ''
+                  }
+                  routerLink={appPage.url}
+                  routerDirection='none'
+                  lines='none'
+                  detail={false}
+                >
+                  <IonIcon
+                    aria-hidden='true'
+                    slot='start'
+                    ios={appPage.iosIcon}
+                    md={appPage.mdIcon}
+                  />
                   <IonLabel>{appPage.title}</IonLabel>
                 </IonItem>
               </IonMenuToggle>
-            );
+            )
           })}
         </IonList>
 
-        <IonList id="labels-list">
+        {/* <IonList id='labels-list'>
           <IonListHeader>Labels</IonListHeader>
           {labels.map((label, index) => (
-            <IonItem lines="none" key={index}>
-              <IonIcon aria-hidden="true" slot="start" icon={bookmarkOutline} />
+            <IonItem lines='none' key={index}>
+              <IonIcon aria-hidden='true' slot='start' icon={bookmarkOutline} />
               <IonLabel>{label}</IonLabel>
             </IonItem>
           ))}
-        </IonList>
+        </IonList> */}
       </IonContent>
     </IonMenu>
-  );
-};
+  )
+}
 
-export default Menu;
+export default Menu
