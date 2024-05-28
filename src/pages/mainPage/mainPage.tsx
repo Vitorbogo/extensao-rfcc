84bf2c51766b4256cb49aa329b8c54d7f7449a4e
Vitorbogo <vitorbogo@edu.univali.br>
2024-05-28 12:36:27 -0300

    app layout #10

--- src/pages/mainPage/mainPage.tsx
+++ src/pages/mainPage/mainPage.tsx
@@ -1,38 +1,32 @@
-import {
-  IonButtons,
-  IonContent,
-  IonHeader,
-  IonMenuButton,
-  IonPage,
-  IonTitle,
-  IonToolbar,
-} from '@ionic/react'
-import { useParams } from 'react-router'
-import ExploreContainer from '../../components/ExploreContainer'
+import { IonCol, IonGrid, IonRow } from '@ionic/react'
+import AppLayout from '../../components/appLayout'
 
 const MainPage: React.FC = () => {
-  const { name } = useParams<{ name: string }>()
-
   return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonButtons slot='start'>
-            <IonMenuButton />
-          </IonButtons>
-          <IonTitle>{name}</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-
-      <IonContent fullscreen>
-        <IonHeader collapse='condense'>
-          <IonToolbar>
-            <IonTitle size='large'>{name}</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name={name} />
-      </IonContent>
-    </IonPage>
+    <AppLayout title='Home'>
+      <IonGrid>
+        <IonRow>
+          <IonCol size='6'>
+            <div style={{ backgroundColor: 'pink', height: '100px' }}></div>
+          </IonCol>
+          <IonCol size='6'>
+            <div style={{ backgroundColor: 'pink', height: '100px' }}></div>
+          </IonCol>
+          <IonCol size='6'>
+            <div style={{ backgroundColor: 'pink', height: '100px' }}></div>
+          </IonCol>
+          <IonCol size='6'>
+            <div style={{ backgroundColor: 'pink', height: '100px' }}></div>
+          </IonCol>
+          <IonCol size='6'>
+            <div style={{ backgroundColor: 'pink', height: '100px' }}></div>
+          </IonCol>
+          <IonCol size='6'>
+            <div style={{ backgroundColor: 'pink', height: '100px' }}></div>
+          </IonCol>
+        </IonRow>
+      </IonGrid>
+    </AppLayout>
   )
 }
 
