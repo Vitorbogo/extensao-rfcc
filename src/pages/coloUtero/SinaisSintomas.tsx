a6d041057434f8d2c777788d5877a623c095ceb5
Eduardo L. Oliani <olianiedu@gmail.com>
2024-06-24 08:59:21 -0300

    página de notícias criada

--- src/pages/coloUtero/SinaisSintomas.tsx
+++ src/pages/coloUtero/SinaisSintomas.tsx
@@ -1,80 +1,90 @@
-import React, { useState, useEffect } from 'react';
-import { IonContent, IonSpinner } from '@ionic/react';
-import styled from 'styled-components';
-import AppLayout from '../../components/appLayout';
-import { useHistory } from 'react-router';
-import { fetchCollectionForm } from '../../services/pagesInfo';
+import React from 'react'
+import { IonContent } from '@ionic/react'
+import styled from 'styled-components'
+import AppLayout from '../../components/appLayout'
+import { useHistory } from 'react-router'
 
 const SinaisSintomasColoUtero: React.FC = () => {
-  const history = useHistory();
-  const [questions, setQuestions] = useState<string[]>([]);
-  const [loading, setLoading] = useState<boolean>(true);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const data = await fetchCollectionForm('sinais_sintomas');
-        setQuestions(data);
-      } catch (error) {
-        console.error(error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchData();
-  }, []);
+  const history = useHistory()
 
   return (
     <AppLayout title='Sinais e Sintomas' history={history}>
       <IonContent>
-        {loading ? (
-          <LoadingContainer>
-            <SpinnerWrapper>
-              <IonSpinner name="crescent" />
-            </SpinnerWrapper>
-          </LoadingContainer>
-        ) : (
-          <Container>
-            <Title>Está com algum desses sintomas?</Title>
-            {questions.map((question, index) => (
-              <Question key={index}>
-                <Column>{question}</Column>
-                <Column>
-                  <WrapperOption>
-                    <Option>
-                      <input type='radio' id={`question_${index}_sim`} name={`question_${index}`} value='sim' />
-                      <label htmlFor={`question_${index}_sim`}> Sim</label>
-                    </Option>
-                    <Option>
-                      <input type='radio' id={`question_${index}_nao`} name={`question_${index}`} value='nao' />
-                      <label htmlFor={`question_${index}_nao`}> Não</label>
-                    </Option>
-                  </WrapperOption>
-                </Column>
-              </Question>
-            ))}
-            <Button>Concluir</Button>
-          </Container>
-        )}
+        <Container>
+          <Title>Está com algum desses sintomas?</Title>
+
+          <Question>
+            <Column>Sangramento e secreção vaginal anormal</Column>
+            <Column>
+              <WrapperOption>
+                <Option>
+                  <input type='radio' id='question_one_sim' name='question_one' value='sim' />
+                  <label htmlFor={'question_one_sim'}> Sim</label>
+                </Option>
+                <Option>
+                  <input type='radio' id='question_one_two' name='question_one' value='nao' />
+                  <label htmlFor={'question_one_two'}> Não</label>
+                </Option>
+              </WrapperOption>
+            </Column>
+          </Question>
+
+          <Question>
+            <Column>Dor abdominal associada com queixas urinárias ou intestinais</Column>
+            <Column>
+              <WrapperOption>
+                <Option>
+                  <input type='radio' id='question_two_sim' name='question_two' value='sim' />
+                  <label htmlFor={'question_two_sim'}> Sim</label>
+                </Option>
+                <Option>
+                  <input type='radio' id='question_two_no' name='question_two' value='nao' />
+                  <label htmlFor={'question_two_no'}> Não</label>
+                </Option>
+              </WrapperOption>
+            </Column>
+          </Question>
+
+          <Question>
+            <Column>Sangramento menstrual mais prolongado</Column>
+            <Column>
+              <WrapperOption>
+                <Option>
+                  <input type='radio' id='question_three_sim' name='question_three' value='sim' />
+                  <label htmlFor={'question_three_sim'}> Sim</label>
+                </Option>
+                <Option>
+                  <input type='radio' id='question_three_no' name='question_three' value='nao' />
+                  <label htmlFor={'question_three_no'}> Não</label>
+                </Option>
+              </WrapperOption>
+            </Column>
+          </Question>
+
+          <Question>
+            <Column>Sangramento após a relação sexual e dores durante a relação</Column>
+            <Column>
+              <WrapperOption>
+                <Option>
+                  <input type='radio' id='question_four_sim' name='question_four' value='sim' />
+                  <label htmlFor={'question_four_sim'}> Sim</label>
+                </Option>
+                <Option>
+                  <input type='radio' id='question_four_no' name='question_four' value='nao' />
+                  <label htmlFor={'question_four_no'}> Não</label>
+                </Option>
+              </WrapperOption>
+            </Column>
+          </Question>
+
+          <Button>Concluir</Button>
+        </Container>
       </IonContent>
     </AppLayout>
-  );
-};
+  )
+}
 
-export default SinaisSintomasColoUtero;
-
-const LoadingContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;`;
-
-const SpinnerWrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100%;`;
+export default SinaisSintomasColoUtero
 
 const Container = styled.div`
   border-radius: 10px;
@@ -85,25 +95,25 @@ const Container = styled.div`
   width: 100%;
   align-items: center;
   justify-content: center;
-`;
+`
 
 const Title = styled.div`
   font-size: 20px;
   margin-bottom: 45px;
-`;
+`
 
 const Question = styled.div`
   display: flex;
   flex-direction: row;
   width: 100%;
   margin-bottom: 45px;
-`;
+`
 
 const Column = styled.div`
   display: flex;
   flex-direction: column;
   width: 50%;
-`;
+`
 
 const Button = styled.button`
   width: 150px;
@@ -112,11 +122,11 @@ const Button = styled.button`
   background-color: pink;
   border-radius: 10px;
   color: white;
-`;
+`
 
 const WrapperOption = styled.div`
   display: flex;
   flex-direction: column;
-`;
+`
 
-const Option = styled.div``;
+const Option = styled.div``
--- src/services/pagesInfo.ts
+++ src/services/pagesInfo.ts
@@ -2,19 +2,24 @@ import { collection, doc, getDoc, getDocs, QuerySnapshot, DocumentData } from 'f
 import { firestore } from '../firebase/firebase';
 
 export async function fetchFieldFromDocument(idDocumento: string, campoName: string): Promise<any> {
-    const docRef = doc(firestore, 'info_telas', idDocumento);
-    const docSnapshot = await getDoc(docRef);
+    try {
+        const docRef = doc(firestore, 'info_telas', idDocumento);
+        const docSnapshot = await getDoc(docRef);
 
-    if (!docSnapshot.exists()) {
-        throw new Error(`Documento com ID "${idDocumento}" não encontrado na coleção.`);
+        if (docSnapshot.exists()) {
+            const data = docSnapshot.data();
+            if (data && campoName in data) {
+                return data[campoName];
+            } else {
+                throw new Error('Não foi possível encontrar o campo ' + campoName + ' no documento');
+            }
+        } else {
+            throw new Error('ID ' + idDocumento + ' não encontrado na coleção');
+        }
+    } catch (error: any) {
+        console.error("Erro na busca do documento:", error);
+        throw new Error("Erro ao buscar o documento: " + error.message);
     }
-
-    const data = docSnapshot.data();
-    if (!data || data[campoName] === undefined) {
-        throw new Error(`Campo "${campoName}" não encontrado no documento.`);
-    }
-
-    return data[campoName];
 }
 
 export async function fetchCollectionForm(documentId: string): Promise<any[]> {
