0f397db4d8a73794962987935a081d53a5029456
Rubia Roberta Cacemiro de Souza <rubia@energysimple.com.br>
2024-07-03 17:53:51 -0300

    issue/14

--- src/pages/cancerDeMama/FatoresProtecao.tsx
+++ src/pages/cancerDeMama/FatoresProtecao.tsx
@@ -1,129 +1,115 @@
-import React from 'react'
-import { IonContent } from '@ionic/react'
-import styled from 'styled-components'
-import { useHistory } from 'react-router'
-import AppLayout from '../../components/appLayout'
+import React, { useEffect, useState } from 'react';
+import { IonContent, IonSpinner } from '@ionic/react';
+import styled from 'styled-components';
+import AppLayout from '../../components/appLayout';
+import { useHistory } from 'react-router';
+import { fBuscaInfoPages } from '../../services/pagesInfo';
 
 const ContentBox = styled.div`
   text-align: justify;
   border-radius: 10px;
   padding: 20px;
-  margin: 70px 20px 20px 20px;
+  margin: 0px 20px 20px 20px;
   color: var(--ion-color-text);
 
   & a {
     text-decoration: none;
     color: pink;
   }
-`
+`;
+
+const LoadingContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const SpinnerWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+`;
+
+const TitleDiv = styled.div`
+  margin-bottom: -15px;
+`;
 
 const FatoresProtecao: React.FC = () => {
-  const history = useHistory()
+  const history = useHistory();
+  const [contentData, setContentData] = useState<any>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const data = await fBuscaInfoPages('fatores_protecao_cancer_mama');
+        setContentData(data);
+      } catch (error) {
+        console.error('Erro ao buscar dados do banco:', error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchData();
+  }, []);
 
   return (
-    <AppLayout title='Fatores de Proteção' history={history}>
+    <AppLayout title="Fatores de Proteção" history={history}>
       <IonContent>
-        <ContentBox>
-          <img src={'assets/images/fatores_protecao.jpg'} width={'350px'} />
-
-          <p>
-            <strong>Os principais fatores para reduzir o risco do câncer de mama são: </strong>
-          </p>
-          <p>
-            <strong>Exercícios físicos regulares: </strong> A atividade física ajuda a manter um peso saudável,
-            reduzindo o risco de obesidade e os níveis elevados de estrogênio associados, ambos fatores de risco para o
-            câncer de mama. Exercícios também fortalecem o sistema imunológico.
-          </p>
-          <p>
-            <strong>Dieta equilibrada e saudável: </strong> Uma dieta rica em vegetais, frutas, grãos integrais e baixa
-            em gorduras saturadas pode ajudar a reduzir o risco de câncer de mama. Alimentos antioxidantes e ricos em
-            fibras são particularmente benéficos.
-          </p>
-          <p>
-            <strong>Manutenção de um peso corporal saudável: </strong> Estar acima do peso ou obeso, especialmente após
-            a menopausa, aumenta o risco de câncer de mama. Manter um peso saudável pode ajudar a diminuir esse risco.
-          </p>
-          <p>
-            <strong>Limitar o consumo de álcool: </strong> O consumo de álcool está ligado a um risco aumentado de
-            câncer de mama. Reduzir a ingestão de álcool pode ser uma medida preventiva eficaz.
-          </p>
-          <p>
-            <strong>Limitar o uso da terapia de reposição hormonal prolongada (TRH): </strong> O uso de TRH,
-            especialmente a combinação de estrogênio e progesterona, foi associado a um aumento do risco. Limitar o
-            tempo de uso pode ser benéfico. Quem a utilizar, utilizar por no máximo 5 anos. Não se pode utilizar TRH por
-            toda a vida depois da menopausa!!!
-          </p>
-          <p>
-            <strong>Educação sobre riscos genéticos e histórico familiar: </strong> Conhecer e entender o histórico
-            familiar de câncer de mama e de mais tipos de cânceres (ovário, próstata, pâncreas, intestino...) pode
-            ajudar na adoção de estratégias de prevenção e no rastreamento precoce, especialmente para aqueles com
-            mutações genéticas conhecidas que aumentam o risco.
-          </p>
-          <p>
-            <strong>Gravidez, especialmente quando ocorre antes dos 30 anos: </strong> está associada a uma redução no
-            risco de câncer de mama. Existem várias razões biológicas por trás dessa conexão.
-          </p>
-          <p>
-            <ul>
-              <li>
-                Alterações Hormonais Durante a Gravidez: Durante a gravidez, o corpo da mulher passa por mudanças
-                hormonais significativas. Essas alterações podem ter um efeito protetor sobre as células mamárias,
-                tornando-as menos suscetíveis a se transformarem em células cancerígenas.
-              </li>
-              <li>
-                Diferenciação das Células Mamárias: A gravidez leva à diferenciação completa das células mamárias. Isso
-                significa que as células mamárias amadurecem e atingem um estado em que são menos propensas a sofrer
-                mutações que podem levar ao câncer.
-              </li>
-              <li>
-                Menor Exposição a Ciclos Menstruais: Mulheres que engravidam têm menos ciclos menstruais ao longo da
-                vida. Cada ciclo menstrual envolve um aumento nos níveis de estrogênio e progesterona, que podem
-                estimular o crescimento de células mamárias, incluindo células anormais ou pré-cancerígenas. Portanto,
-                menos ciclos podem significar um risco reduzido.
-              </li>
-            </ul>
-          </p>
-          <p>
-            <strong>Amamentação: </strong> A amamentação, que muitas vezes segue a gravidez, também tem sido associada a
-            uma redução no risco de câncer de mama. A amamentação por um período prolongado pode prolongar o período de
-            alterações hormonais benéficas e contribuir para a eliminação de células com potencial para danos ao DNA.
-          </p>
-          <p>
-            Estes fatores de proteção não garantem a prevenção do câncer de mama, mas podem significativamente reduzir o
-            risco. É importante combinar estes hábitos saudáveis com consultas regulares ao médico e discussões
-            informadas sobre riscos e estratégias de prevenção individualizadas.
-          </p>
-          <p>
-            <strong>Referência: </strong>
-          </p>
-          <p>
-            <ul>
-              <li>
-                {' '}
-                IARC,{' '}
-                <a href='https://gco.iarc.fr/' target='_blank'>
-                  https://gco.iarc.fr/
-                </a>
-              </li>
-              <li>
-                {' '}
-                INCA,{' '}
-                <a href='https://www.gov.br/inca/pt-br/assuntos/cancer/numeros' target='_blank'>
-                  https://www.gov.br/inca/pt-br/assuntos/cancer/numeros
-                </a>
-              </li>
-              <li>
-                {' '}
-                <a href='http://BreastCancer.org' target='_blank'>
-                  BreastCancer.org
-                </a>
-              </li>
-            </ul>
-          </p>
-        </ContentBox>
+        {loading ? (
+          <LoadingContainer>
+            <SpinnerWrapper>
+              <IonSpinner name="crescent" />
+            </SpinnerWrapper>
+          </LoadingContainer>
+        ) : (
+          <ContentBox>
+            {renderContent(contentData?.text)}
+          </ContentBox>
+        )}
       </IonContent>
     </AppLayout>
-  )
+  );
+};
+
+function renderContent(textData: any[]) {
+  return textData.map((item: any, index: number) => {
+    if (item.type === 'title') {
+      return (
+        <TitleDiv key={index}>
+        <strong>{item.content}</strong>
+        </TitleDiv>
+      );
+    } else if (item.type === 'paragraph') {
+      return <p key={index}>{item.content}</p>;
+    } else if (item.type === 'list') {
+      return (
+        <ul key={index}>
+          {item.content.map((info: string, idx: number) => (
+            <li key={idx}>{info}</li>
+          ))}
+        </ul>
+      );
+    } else if (item.type === 'references') {
+      return (
+        <div key={index}>
+          <strong>{item.content}</strong>
+          <ul>
+            {item.references.map((ref: any, idx: number) => (
+              <li key={idx}>
+                {ref.name}: <a href={ref.url} target="_blank" rel="noopener noreferrer">{ref.url}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      );
+    }
+    return null;
+  });
 }
 
-export default FatoresProtecao
+
+export default FatoresProtecao;
--- src/pages/cancerDeMama/FatoresRisco.tsx
+++ src/pages/cancerDeMama/FatoresRisco.tsx
@@ -1,14 +1,15 @@
-import React from 'react'
-import { IonContent } from '@ionic/react'
+import React, { useEffect, useState } from 'react';
+import { IonContent, IonSpinner } from '@ionic/react';
 import styled from 'styled-components'
 import { useHistory } from 'react-router'
 import AppLayout from '../../components/appLayout'
+import { fBuscaInfoPages } from '../../services/pagesInfo';
 
 const ContentBox = styled.div`
   text-align: justify;
   border-radius: 10px;
   padding: 20px;
-  margin: 70px 20px 20px 20px;
+  margin: 0px 20px 20px 20px;
   color: var(--ion-color-text);
 
   & a {
@@ -17,108 +18,97 @@ const ContentBox = styled.div`
   }
 `
 
+const LoadingContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const SpinnerWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+`;
+
+const TitleDiv = styled.div`
+  margin-bottom: -15px;
+`;
+
 const FatoresRisco: React.FC = () => {
-  const history = useHistory()
+  const history = useHistory();
+  const [contentData, setContentData] = useState<any>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const data = await fBuscaInfoPages('fatores_protecao_cancer_mama');
+        setContentData(data);
+      } catch (error) {
+        console.error('Erro ao buscar dados do banco:', error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchData();
+  }, []);
 
   return (
     <AppLayout title='Fatores de Risco' history={history}>
       <IonContent>
-        <ContentBox>
-          Os principais fatores de risco para o desenvolvimento de câncer de mama incluem ser mulher e envelhecer. Vamos
-          explicar melhor:
-          <p>
-            <strong>Sexo:</strong> Ser do sexo feminino é o principal fator de risco para o desenvolvimento de câncer de
-            mama (pois homens também têm câncer de mama). São estimados cerca de 100 casos de câncer de mama em mulheres
-            para um caso em homem. No Brasil, são esperados para esse ano cerca de 73.610 casos de câncer de mama em
-            mulheres e 730 casos de câncer de mama em homens.
-          </p>
-          <p>
-            <strong>Envelhecimento:</strong> A idade também desempenha um papel importante. O risco aumenta com a idade,
-            sendo mais comum em mulheres acima dos 50 anos. As células da mama estão em constante renovação, morrendo e
-            nascendo milhões de células todos os dias em um processo biológico replicativo.
-          </p>
-          <p>
-            Com o envelhecimento o ciclo celular fica desgastado e perde seu controle fino podendo ocorrer mutações no
-            DNA. Uma célula saudável que deveria dar origem a outra célula saudável passa a dar origem a uma célula
-            doente, essa célula doente por sua vez dá origem a uma célula ainda mais doente… e assim sucessivamente. O
-            corpo entra em processo de hiperprodução de células defeituosas gerando uma massa/nódulo em crescimento
-            celular descontrolado.
-          </p>
-          <p>
-            <strong>Histórico Familiar:</strong> Ter parentes de primeiro grau (mãe, irmã ou filha) com câncer de mama
-            aumenta o risco, especialmente se a doença ocorreu em uma idade jovem.
-          </p>
-          <p>
-            <strong>Mutação Genética:</strong> Portadores de mutações nos genes BRCA1, BRCA2, PALB2, TP53, dentre
-            outros, têm um risco significativamente maior de desenvolver câncer de mama.
-          </p>
-          <p>
-            <strong>Exposição a Estrogênio:</strong> Exposições prolongadas ao estrogênio, seja devido a terapia
-            hormonal de reposição, uso de contraceptivos hormonais por longos períodos ou menarca precoce (início da
-            menstruação) e menopausa tardia, podem aumentar o risco.
-          </p>
-          <p>
-            <strong>Menstruação Precoce e Menopausa Tardia:</strong> Começar a menstruar cedo (antes dos 12 anos) ou
-            entrar na menopausa tardiamente (após os 55 anos) pode aumentar o risco pela maior exposição estrogênica ao
-            longo da vida.
-          </p>
-          <p>
-            <strong>Terapia de Reposição Hormonal:</strong> O uso prolongado de terapia hormonal após a menopausa
-            aumenta o risco pelo maior tempo de estímulo replicativo celular pelo hormônio no corpo da mulher.
-          </p>
-          <p>
-            <strong>Densidade Mamária Alta:</strong> Tecido mamário denso pode ocultar cânceres em mamografias,
-            aumentando o risco.
-          </p>
-          <p>
-            <strong>Histórico Pessoal de Câncer de Mama:</strong> Se você já teve câncer de mama em uma mama, tem maior
-            risco de desenvolver na outra mama ou recidiva.
-          </p>
-          <p>
-            <strong>Histórico de Lesões Proliferativas:</strong> Certas lesões não cancerosas da mama, como hiperplasia
-            atípica ou carcinoma lobular in situ, podem aumentar o risco de câncer de mama.
-          </p>
-          <p>
-            <strong>Radiação na Região do Tórax:</strong> Ter recebido tratamento de radiação na área do tórax,
-            especialmente durante a infância para tratamento de linfomas da região torácica, aumenta o risco de câncer
-            de mama.
-          </p>
-          <p>
-            <strong>Obesidade:</strong> Estar acima do peso ou estar obeso, principalmente após a menopausa, está
-            associado a um maior risco.
-          </p>
-          <p>
-            <strong>Consumo de Álcool:</strong> Beber álcool em excesso, mesmo em quantidade moderada, pode aumentar o
-            risco de câncer de mama.
-          </p>
-          <p>
-            <strong>Inatividade Física/Sedentarismo:</strong> A falta de atividade física regular está ligada a um maior
-            risco de câncer de mama.
-          </p>
-          <p>
-            É importante lembrar que ter um ou mais desses fatores de risco não significa necessariamente que você
-            desenvolverá câncer de mama. Muitas pessoas com fatores de risco nunca desenvolvem a doença, enquanto outras
-            sem fatores de risco podem ser diagnosticadas com câncer de mama.
-          </p>
-          <p>
-            Vale a pena ressaltar que alguns desses fatores são modificáveis e exercem grande importância na redução de
-            riscos.
-          </p>
-          <p>
-            <strong>Referência:</strong>
-          </p>
-          <p>
-            <ul>
-              <li>IARC, <a href='https://gco.iarc.fr/' target='_blank'> https://gco.iarc.fr/</a></li>
-              <li>INCA, <a href='#' target='_blank'>Instituto Nacional do Câncer</a></li>
-              <li><a href='BreastCancer.org' target='_blank'> BreastCancer.org</a></li>
-              <li><a href='#' target='_blank'>National Cancer Institute</a></li>
-            </ul>
-          </p>
-        </ContentBox>
+        {loading ? (
+          <LoadingContainer>
+            <SpinnerWrapper>
+              <IonSpinner name="crescent" />
+            </SpinnerWrapper>
+          </LoadingContainer>
+        ) : (
+          <ContentBox>
+            {renderContent(contentData?.text)}
+          </ContentBox>
+        )}
       </IonContent>
     </AppLayout>
   )
+}
+
+function renderContent(textData: any[]) {
+  return textData.map((item: any, index: number) => {
+    if (item.type === 'title') {
+      return (
+        <TitleDiv key={index}>
+          <strong>{item.content}</strong>
+        </TitleDiv>
+      );
+    } else if (item.type === 'paragraph') {
+      return <p key={index}>{item.content}</p>;
+    } else if (item.type === 'list') {
+      return (
+        <ul key={index}>
+          {item.content.map((info: string, idx: number) => (
+            <li key={idx}>{info}</li>
+          ))}
+        </ul>
+      );
+    } else if (item.type === 'references') {
+      return (
+        <div key={index}>
+          <strong>{item.content}</strong>
+          <ul>
+            {item.references.map((ref: any, idx: number) => (
+              <li key={idx}>
+                {ref.name}: <a href={ref.url} target="_blank" rel="noopener noreferrer">{ref.url}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      );
+    }
+    return null;
+  });
 }
 
 export default FatoresRisco
